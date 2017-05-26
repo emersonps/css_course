fb5b5dc1978cde5b726c486c2c68f12e2e9ecf98
EMERSON\Emerson <emersonline2007@gmail.com>
2017-05-25 23:51:43 -0300

    Modelagem do Site do Projeto Site Hotel de Praia: Menu, Slider.

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<html>
+	<head>
+		<title>Projeto Prático - Hotel de Praia</title>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div class="container">
+			
+			<!--Cabeçalho-->
+			<div class="row ei">
+				<div class="coluna_logomarca">
+					<img src="img/logo.png" width="249px" height="49px" alt="Logo Hotel de Praia" />
+				</div>
+				<div class="coluna_menu">
+					<div class="container_menu bordaArredondada" id="teste">
+						<a href="#">Página Inicial</a>
+						<a href="#">Quem Somos</a>
+						<a href="#">Nossos Produtos</a>
+						<a href="#">Localização</a>
+						<a href="#">Fale Conosco</a>	
+					</div>
+				</div>
+			</div>
+			<!-- //Cabeçalho -->
+			
+			<!-- Slider -->
+			<div class="row ei">
+				<div id="containerSlider">
+					<img src="img/slider/img01.jpg" width="925px" height="295px" alt="Sacada - Slider 01" />
+				</div>
+
+			</div>
+			<!-- //Slider -->
+
+			<!-- icones -->
+			<div class="row">
+				icones
+			</div>
+			<!-- //icones -->
+
+			<!-- linha noticias -->
+			<div class="row">
+				Linha Notícias
+			</div>
+			<!-- //linha noticias -->
+
+			<!-- Rodapé -->
+			<div class="row">
+				Rodapé
+			</div>
+			<!-- //Rodapé -->
+
+
+		</div>
+	</body>
+</html>
