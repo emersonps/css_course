a40d21c33709119186ccfbd5a7ab4cc5b167437d
EMERSON\Emerson <emersonline2007@gmail.com>
2017-05-28 03:38:38 -0300

    Modelagem do Site do Projeto Site Hotel de Praia: Área de Ícones, Rodapé, Fonteawesome.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Projeto Prático - Hotel de Praia</title>
 		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
 		<meta charset="utf-8">
 	</head>
 	<body>
@@ -28,27 +29,113 @@
 			<!-- Slider -->
 			<div class="row ei">
 				<div id="containerSlider">
-					<img src="img/slider/img01.jpg" width="925px" height="295px" alt="Sacada - Slider 01" />
+					<img src="img/slider/img02.jpg" width="925px" height="295px" alt="Sacada - Slider 01" />
 				</div>
 
 			</div>
 			<!-- //Slider -->
 
 			<!-- icones -->
-			<div class="row">
-				icones
+			<div class="row ei">
+				<div class="bordaArredondada divBordaCinzaVazada efeitoHover iconeInicial">
+					<div class="marginInterna">
+						<div class="icone">
+							<i class="fa fa-envelope fa-5x"></i>
+						</div>
+						<div class="tituloIcone">
+							Fale Conosco
+						</div>
+					</div>	
+				</div>
+				
+				<div class="bordaArredondada divBordaCinzaVazada efeitoHover iconeInicial">
+					<div class="marginInterna">
+						<div class="icone">
+							<i class="fa fa-map-marker fa-5x"></i>
+						</div>
+						<div class="tituloIcone">
+							Localização
+						</div>
+					</div>	
+				</div>
+				
+				<div class="bordaArredondada divBordaCinzaVazada efeitoHover iconeInicial">
+					<div class="marginInterna">
+						<div class="icone">
+							<i class="fa fa-camera fa-5x"></i>
+						</div>
+						<div class="tituloIcone">
+							Galeria de Fotos
+						</div>
+					</div>	
+				</div>
+				
+				<div class="bordaArredondada divBordaCinzaVazada efeitoHover iconeInicial">
+					<div class="marginInterna">
+						<div class="icone">
+							<i class="fa fa-users fa-5x"></i>
+						</div>
+						<div class="tituloIcone">
+							Redes Sociais
+						</div>
+					</div>	
+				</div>
 			</div>
 			<!-- //icones -->
 
 			<!-- linha noticias -->
-			<div class="row">
-				Linha Notícias
+			<div class="row ei">
+
+				<div class="divBordaCinzaVazada bordaArredondada coluna_areaNoticia">
+					
+					<div class="marginInterna">
+						<div class="titulo ei">Notícias</div>
+					</div>
+					
+					<div class="fundoBrancoTransp ei">
+						<div class="marginInterna">
+							<p><a href="#">Título da Notícia</a></p>
+							<p>Breve descrição da Notícia</p>
+						</div>
+					</div>
+
+					<div class="fundoBrancoTransp ei">
+						<div class="marginInterna">
+							<p><a href="#">Título da Notícia</a></p>
+							<p>Breve descrição da Notícia</p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="coluna_areaProdutos">
+					<div class="coluna_produto01 fundoBrancoTransp bordaArredondada">
+						<div class="marginInterna">
+							<p align="center">
+								<img src="img/produto1.jpg">
+							</p>
+							<p align="center">
+								<a href="#">Nome do Produto</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="coluna_produto02 fundoBrancoTransp bordaArredondada">
+						<div class="marginInterna">
+							<p align="center">
+								<img src="img/produto2.jpg">
+							</p>
+							<p align="center">
+								<a href="#">Nome do Produto</a>
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 			<!-- //linha noticias -->
 
 			<!-- Rodapé -->
-			<div class="row">
-				Rodapé
+			<div class="row fundoBrancoTransp bordaArredondada rodape ie">
+				Informação de Rodapé
 			</div>
 			<!-- //Rodapé -->
 
